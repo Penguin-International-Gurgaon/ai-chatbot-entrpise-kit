a74a1c796333e60ada1d06ebb035f981e54265b1
Dhruv Arya <dhruv166kumar@gmail.com>
2025-06-11 16:25:50 +0530

    Fixed layout (#6)

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,37 +39,31 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      // `next-themes` injects an extra classname to the body element to avoid
-      // visual flicker before hydration. Hence the `suppressHydrationWarning`
-      // prop is necessary to avoid the React hydration mismatch warning.
-      // https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
-      suppressHydrationWarning
-      className={`${geist.variable} ${geistMono.variable}`}
-      style={{
-        '--theme-light': config.theme.LIGHT_THEME_COLOR,
-        '--theme-dark': config.theme.DARK_THEME_COLOR
-      } as React.CSSProperties}
-    >
-      <head>
-        {Object.entries(config.header)
-          .filter(([key]) => !['favicon', 'title', 'subtitle'].includes(key))
-          .map(([key, value]) => (
-            <meta key={key} name={key} content={value} />
-          ))}
-      </head>
-      <body className="antialiased">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Toaster position="top-center" />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+      <html
+        lang="en"
+        suppressHydrationWarning
+        className={`${geist.variable} ${geistMono.variable}`}
+      >
+        <head>
+          {/* Dynamically render meta tags */}
+          {Object.entries(config.header)
+            .filter(([key]) => !['favicon', 'title', 'subtitle'].includes(key))
+            .map(([key, value]) => (
+              <meta key={key} name={key} content={value} />
+            ))}
+        </head>
+        <body className="antialiased">
+          <ThemeProvider
+            attribute="class" // adds `light` or `dark` class to <html> or <body>
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Toaster position="top-center" />
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+
   );
 }
